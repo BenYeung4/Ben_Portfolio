1d04548ff20f39041452f051ade7f6c8fa0c3ad5
Ben <byeungis@gmail.com>
2022-08-29 00:09:17 -0400

    added my name to the nav, but will need to remove and customize more for personal pref after being graded

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -4,6 +4,11 @@ function Navigation(props) {
   const tabs = ["About Me", "Portfolio", "Contact", "Resume"];
   return (
     <ul className="nav">
+      <h2>
+        <a data-testid="link" href="/" className="home">
+          Benjamin Yeung
+        </a>
+      </h2>
       {tabs.map((tab) => (
         <li className="nav-item" key={tab}>
           <a
