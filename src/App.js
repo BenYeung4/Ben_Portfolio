c034c27391be971104a1db126818ee86a1c59f0d
Ben <byeungis@gmail.com>
2022-12-18 23:28:34 -0500

    updated header, need to redesign about, contact, footer, home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <>
       <div>
         <Header />
-        <Footer />
+        {/* <Footer /> */}
       </div>
     </>
   );
