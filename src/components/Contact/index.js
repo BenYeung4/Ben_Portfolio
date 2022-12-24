6e69d0f5482d4399f5fe389a3d1dc21418537cea
Ben <byeungis@gmail.com>
2022-12-24 04:54:07 -0500

    update email, sign up for privacy

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -9,9 +9,9 @@ function Contact() {
     <section className="contact section" id="contact">
       <h2 className="section__title">Lets Grab a Coffee</h2>
       <span className="section__subtitle">Contact Me</span>
-      <div className="contact__conainer container grid">
+      <div className="contact__container container grid">
         <div className="contact__content">
-          <h3 className="contact__title">Talk to Me</h3>
+          <h3 className="contact__title">My Contact Info</h3>
 
           <div className="contact__info">
             <div className="contact__card">
@@ -101,7 +101,7 @@ function Contact() {
               />
             </div>
 
-            <div className="contact__form-div">
+            <div className="contact__form-div contact__form-area">
               <label className="contact__form-tag">Subject</label>
               <textarea
                 name="subject"
