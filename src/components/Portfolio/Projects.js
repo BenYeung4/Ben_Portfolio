727b8656b5916fc3f4cd96c41fb13979d6c540cd
Ben <byeungis@gmail.com>
2023-01-17 00:41:40 -0500

    updated layout design

--- src/components/Portfolio/Projects.js
+++ src/components/Portfolio/Projects.js
@@ -1,15 +1,15 @@
 import React from "react";
 // import projects from "../../projects.json";
 
-import { projectsData } from "./data";
-import { projectsNav } from "./data";
+import projects from "../../projects.json";
+
 import WorkItems from "./WorkItems";
 
 function Projects() {
   return (
     <div>
       <div className="work__filters">
-        {projectsNav.map((item, index) => {
+        {projects.map((item, index) => {
           return (
             <span className="work__item" key={index}>
               {item.name}
@@ -19,7 +19,7 @@ function Projects() {
       </div>
 
       <div className="work__container container grid">
-        {projectsData.map((item) => {
+        {projects.map((item) => {
           return <WorkItems item={item} key={item.id} />;
         })}
       </div>
--- src/components/Portfolio/WorkItems.js
+++ src/components/Portfolio/WorkItems.js
@@ -4,10 +4,29 @@ function WorkItems({ item }) {
   return (
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
-      <h3 className="work__title">{item.name}</h3>
-      <a href="something" className="work__button">
-        demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
-      </a>
+      <h3 className="work__title" style={{ textAlign: "center" }}>
+        {item.name}
+      </h3>
+      <h9 className="work__description">{item.description}</h9>
+      <h5 className="work__skills">{item.skills}</h5>
+      <div className="portfolio_icon">
+        <a
+          href={item.github}
+          target="_blank"
+          rel="noreferrer"
+          className="github__icon"
+        >
+          <i class="uil uil-github-alt"></i>
+        </a>
+        <a
+          href={item.deployedapp}
+          target="_blank"
+          rel="noreferrer"
+          className="deployproject_icon"
+        >
+          <i class="uil uil-location-arrow"></i>
+        </a>
+      </div>
     </div>
   );
 }
